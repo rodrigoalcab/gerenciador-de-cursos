b07f95459a3d81400460ba93233858d6329b4903
Rodrigo Cabral <rodrigoalcab@gmail.com>
2019-11-30 19:39:01 +0000

    Botão de Alterar Curso Adicionado

---
+++ view/cursos/formulario.php
@@ -0,0 +1,18 @@
+<?php
+require __DIR__ . '/../inicio-html.php';
+?>
+
+    <form action="/salvar-curso" method="post">
+        <div class="form-group">
+            <label for="descricao">Descrição</label>
+            <input type="text"
+                   id="descricao"
+                   name="descricao"
+                   class="form-control"
+                   value="<?= isset($curso) ? $curso->getDescricao() : ''; ?>">
+        </div>
+        <button class="btn btn-primary">Salvar</button>
+    </form>
+<?php
+require __DIR__ . '/../fim-html.php';
+?>
