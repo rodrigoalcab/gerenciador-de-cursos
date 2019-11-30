81ae4a58227b6ee605656ece3fb1301cef6df166
Rodrigo Cabral <rodrigoalcab@gmail.com>
2019-11-30 20:19:42 +0000

    Adição da função de atualização de cursos

--- src/Controller/Persistencia.php
+++ src/Controller/Persistencia.php
@@ -30,7 +30,19 @@ class Persistencia implements InterfaceControladorRequisicao
         );
         $curso = new Curso();
         $curso->setDescricao($descricao);
+
+        $id = filter_input(
+            INPUT_GET,
+            'id',
+            FILTER_VALIDATE_INT
+        );
+
+        if (!is_null($id) && $id !== false) {
+            $curso->setId($id);
+            $this->entityManager->merge($curso);
+        } else {
         $this->entityManager->persist($curso);
+        }
         $this->entityManager->flush();
 
         header('Location: /listar-cursos', true, 302);
--- view/cursos/formulario.php
+++ view/cursos/formulario.php
@@ -2,7 +2,7 @@
 require __DIR__ . '/../inicio-html.php';
 ?>
 
-    <form action="/salvar-curso" method="post">
+    <form action="/salvar-curso<?= isset($curso) ? '?id=' . $curso->getId() : '';?>"  method="post">
         <div class="form-group">
             <label for="descricao">Descrição</label>
             <input type="text"
