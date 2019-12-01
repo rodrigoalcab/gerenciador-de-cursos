0a1a6fe496aedb8b95b26b6c5744a972f4d8c390
Rodrigo Cabral <rodrigoalcab@gmail.com>
2019-12-01 17:35:52 +0000

    Processo de login criado

--- config/routes.php
+++ config/routes.php
@@ -3,13 +3,17 @@
 use Alura\Cursos\Controller\Exclusao;
 use Alura\Cursos\Controller\FormularioEdicao;
 use Alura\Cursos\Controller\FormularioInsercao;
+use Alura\Cursos\Controller\FormularioLogin;
 use Alura\Cursos\Controller\ListarCursos;
 use Alura\Cursos\Controller\Persistencia;
+use Alura\Cursos\Controller\RealizaLogin;
 
 return [
     '/listar-cursos' => ListarCursos::class,
     '/novo-curso' => FormularioInsercao::class,
     '/salvar-curso' => Persistencia::class,
     '/excluir-curso' => Exclusao::class,
-    '/alterar-curso' => FormularioEdicao::class
+    '/alterar-curso' => FormularioEdicao::class,
+    '/login' => FormularioLogin::class,
+    '/realiza-login' => RealizaLogin::class
 ];
